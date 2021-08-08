adc9967326aefe73b3086eb519a18520b4d0dc7b
Vignesh Shanmugasundaram <vignesh@candidteams.com>
2021-08-08 22:00:57 +0530

    Dashboard page

--- app/javascript/app/containers/dashboard/container.jsx
+++ app/javascript/app/containers/dashboard/container.jsx
@@ -3,12 +3,112 @@
 import React, { useEffect, useReducer, useState } from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
+import { Link } from "@reach/router";
+import { ArrowNarrowRightIcon, CheckCircleIcon, UserAddIcon } from "@heroicons/react/solid";
 
 export default function ({ children }) {
   return (
-    <>
-      <h1>Dashboard container</h1>
-      {children}
-    </>
+    <div className="w-full h-full flex items-center justify-center pt-20">
+      <div className="container mx-auto">
+        <div className="w-8/12 mx-auto">
+          <h1 className="text-2xl font-medium">Welcome Vignesh Shanmugasundaram!</h1>
+          <p className="mt-3 text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, quis laboriosam nostrum placeat vel
+            delectus ad fuga animi modi minus, omnis deserunt dolorem, nam aspernatur eligendi unde doloribus impedit
+            temporibus.
+          </p>
+          <div className="flex mt-10">
+            <div className="w-6/12 flex p-3 rounded cursor-pointer hover:bg-green-500 hover:bg-opacity-10 transition-colors">
+              <div className="w-24 h-24 bg-pink-600 rounded-lg flex-shrink-0 flex items-center justify-center">
+                <UserAddIcon className="w-10 h-10 text-white"></UserAddIcon>
+              </div>
+              <div className="pl-3">
+                <div className="font-medium">
+                  <div className="inline-block align-middle">Invite users</div>
+                  <div className="inline-block align-middle ml-1">
+                    <ArrowNarrowRightIcon className="w-5 h-5"></ArrowNarrowRightIcon>
+                  </div>
+                </div>
+                <p className="text-gray-500">
+                  Qui asperiores aborum repudiandae cum repellendus sed nihil veniam eaque quos ea rerum ipsa
+                  reprehenderit non!
+                </p>
+              </div>
+            </div>
+            <Link to="/retro" className="block w-6/12">
+              <div className="w-full flex p-3 rounded cursor-pointer hover:bg-green-500 hover:bg-opacity-10 transition-colors">
+                <div className="w-24 h-24 bg-blue-500 rounded-lg flex-shrink-0 flex items-center justify-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="36"
+                    height="36"
+                    fill="#fff"
+                    class="bi bi-kanban-fill"
+                    viewBox="0 0 16 16">
+                    <path d="M2.5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2h-11zm5 2h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1zm-5 1a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V3zm9-1h1a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1z" />
+                  </svg>
+                </div>
+                <div className="pl-3">
+                  <div className="font-medium">
+                    <div className="inline-block align-middle">Create a retrospective board</div>
+                    <div className="inline-block align-middle ml-1">
+                      <ArrowNarrowRightIcon className="w-5 h-5"></ArrowNarrowRightIcon>
+                    </div>
+                  </div>
+                  <p className="text-gray-500">
+                    Laborum repudiandae cum repellendus sed nihil veniam eaque quos ea rerum ipsa reprehenderit non!
+                  </p>
+                </div>
+              </div>
+            </Link>
+          </div>
+
+          <div className="flex mt-10">
+            <Link to="/poker" className="block w-6/12">
+              <div className="w-full flex p-3 rounded cursor-pointer hover:bg-green-500 hover:bg-opacity-10 transition-colors">
+                <div className="w-24 h-24 bg-yellow-500 rounded-lg flex-shrink-0 flex items-center justify-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="36"
+                    height="36"
+                    fill="#fff"
+                    class="bi bi-suit-club-fill"
+                    viewBox="0 0 16 16">
+                    <path d="M11.5 12.5a3.493 3.493 0 0 1-2.684-1.254 19.92 19.92 0 0 0 1.582 2.907c.231.35-.02.847-.438.847H6.04c-.419 0-.67-.497-.438-.847a19.919 19.919 0 0 0 1.582-2.907 3.5 3.5 0 1 1-2.538-5.743 3.5 3.5 0 1 1 6.708 0A3.5 3.5 0 1 1 11.5 12.5z" />
+                  </svg>
+                </div>
+                <div className="pl-3">
+                  <div className="font-medium">
+                    <div className="inline-block align-middle">Create a planning poker board</div>
+                    <div className="inline-block align-middle ml-1">
+                      <ArrowNarrowRightIcon className="w-5 h-5"></ArrowNarrowRightIcon>
+                    </div>
+                  </div>
+                  <p className="text-gray-500">
+                    Saepe laborum repudiandae cum repellendus sed nihil veniam eaque quos ea rerum ipsa reprehenderit
+                    non!
+                  </p>
+                </div>
+              </div>
+            </Link>
+            <div className="w-6/12 flex p-3 rounded cursor-pointer hover:bg-green-500 hover:bg-opacity-10 transition-colors opacity-50">
+              <div className="w-24 h-24 bg-green-500 rounded-lg flex-shrink-0 flex items-center justify-center">
+                <CheckCircleIcon className="w-10 h-10 text-white"></CheckCircleIcon>
+              </div>
+              <div className="pl-3">
+                <div className="font-medium">
+                  <div className="inline-block align-middle">Create a checkin</div>
+                  <div className="inline-block align-middle ml-1">
+                    <ArrowNarrowRightIcon className="w-5 h-5"></ArrowNarrowRightIcon>
+                  </div>
+                </div>
+                <p className="text-sm text-yellow-700">Coming soon!</p>
+                <p className="text-gray-500">Qui asperiores, odit velit nesciunt impedit consectetur saepe</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- app/javascript/app/containers/poker/list.jsx
+++ app/javascript/app/containers/poker/list.jsx
@@ -2,14 +2,13 @@
 
 import React, { useEffect, useReducer, useState } from "react";
 import ReactDOM from "react-dom";
-import PropTypes from "prop-types";
 import pluralize from "pluralize";
 import { Link } from "@reach/router";
 import truncate from "../../lib/truncate";
 
 import { Primary as PrimaryButton } from "../../components/button";
 import CreateBoard from "./modals/create_board";
-
+import EmptyData from "images/empty.png";
 import Poker from "../../services/poker";
 
 export default function ({ children }) {
@@ -79,43 +78,62 @@ export default function ({ children }) {
           </div>
 
           <div className="w-full">
-            <div className="grid grid-cols-2 grid-gap-3">
-              {data.state == "loaded" &&
-                data.boards &&
-                data.boards.length > 0 &&
-                data.boards.map((board) => {
-                  return (
-                    <div className="p-3" key={board.id}>
-                      <Link to={`/poker/board/${board.id}`}>
-                        <div className="p-5 bg-white rounded shadow">
-                          <p className="text-lg font-medium">{board.name}</p>
-                          <div className="flex text-gray-500 items-center">
-                            <span className="text-sm ">{board.createdAt}</span>
-                            <div className="middot bg-gray-500"></div>
-                            <span className="text-sm">{pluralize("player", board.participantsCount, true)}</span>
-                            <div className="middot bg-gray-500"></div>
-                            <span className="text-sm">{truncate(board.availableVotesString, 20)}</span>
-                          </div>
+            {data.state == "loaded" && data.boards && (
+              <>
+                {data.boards.length == 0 && (
+                  <>
+                    <div className="p-10 mt-10 w-4/12 mx-auto text-center">
+                      <img src={EmptyData} alt="No poker board found" className="w-f" />
+                      <div className="mt-5">
+                        <span className="text-gray-500"> No poker board found, </span>
+                        <span
+                          className="text-green-500 cursor-pointer hover:underline"
+                          onClick={() => setShowCreateBoard(true)}>
+                          create one here.
+                        </span>
+                      </div>
+                    </div>
+                  </>
+                )}
+
+                {data.boards.length > 0 && (
+                  <div className="grid grid-cols-2 grid-gap-3">
+                    {data.boards.map((board) => {
+                      return (
+                        <div className="p-3" key={board.id}>
+                          <Link to={`/poker/board/${board.id}`}>
+                            <div className="p-5 bg-white rounded shadow">
+                              <p className="text-lg font-medium">{board.name}</p>
+                              <div className="flex text-gray-500 items-center">
+                                <span className="text-sm ">{board.createdAt}</span>
+                                <div className="middot bg-gray-500"></div>
+                                <span className="text-sm">{pluralize("player", board.participantsCount, true)}</span>
+                                <div className="middot bg-gray-500"></div>
+                                <span className="text-sm">{truncate(board.availableVotesString, 20)}</span>
+                              </div>
+                            </div>
+                          </Link>
                         </div>
-                      </Link>
+                      );
+                    })}
+                  </div>
+                )}
+              </>
+            )}
+            {data.state == "loading" && (
+              <div className="grid grid-cols-2 grid-gap-3">
+                {_.times(6, (n) => {
+                  return (
+                    <div className="p-3" key={n}>
+                      <div className="p-5 bg-white rounded shadow">
+                        <div className="w-10/12 h-3 mb-3.5 bg-gray-200 rounded-xl animate-pulse"></div>
+                        <div className="w-9/12 h-2 bg-gray-200 rounded-xl animate-pulse"></div>
+                      </div>
                     </div>
                   );
                 })}
-              {data.state == "loading" && (
-                <>
-                  {_.times(6, (n) => {
-                    return (
-                      <div className="p-3" key={n}>
-                        <div className="p-5 bg-white rounded shadow">
-                          <div className="w-10/12 h-3 mb-3.5 bg-gray-200 rounded-xl animate-pulse"></div>
-                          <div className="w-9/12 h-2 bg-gray-200 rounded-xl animate-pulse"></div>
-                        </div>
-                      </div>
-                    );
-                  })}
-                </>
-              )}
-            </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
